910a9e3ceef27c89c515a86af21176f02649b9c5
rendzhi <rendzhi@gmail.com>
2024-01-08 22:57:59 +0700

    start

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-//TODO server auth api;
+//TODO server auth api ;
 const isAuthenticated = true;
 
 export function middleware(request: NextRequest) {
